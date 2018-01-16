928ba27318415d736a1bd6f18154a1dce9d19dee
scull1916 <kpscully@gmail.com>
2018-01-15 21:01:52 -0500

    timer functioning to stop on button click; capture value of button clicked; determine if button click is correct or not

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,13 +7,24 @@ $(document).ready(function()
 var t = 30;
 var timeOut;
 var userPick = "place holder";
+var correctGuess = 0;
+var incorrectGuess = 0;
 var questOne =
 {
 	num: 1,
 	qInt: "Fill in the blank",
-	qText: "Hello. My name is ________. Prepare to die.",
+	qText: "Hello. My name is ________. You killed my father. Prepare to die.",
 	qAns: ["Fezzik", "Westley", "Buttercup", "Inigo Montoya"],
 	cAns: "Inigo Montoya"
+
+
+};
+
+var questTwo =
+{
+	num: 2,
+	qInt:
+	qText: "Who said: 'You have done well today, Westley. Sleep well, I will most likely kill you in the morning."
 
 
 };
@@ -31,8 +42,9 @@ for (i=0; i<questOne.qAns.length; i++)
 {
 	
 	//Push an answer to a button
-	$("<button></button>").appendTo(".questions").addClass("btn btn-success btn-lg btn-block").text(questOne.qAns[i]);
+	$("<button></button>").appendTo(".questions").addClass("btn btn-success btn-lg btn-block").attr("value",questOne.qAns[i]).text(questOne.qAns[i]);
 	console.log(questOne.qAns[i]);
+
 }
 
 function timer()
@@ -58,8 +70,21 @@ $(".btn").on("click", function()
 {
 	
 	clearTimeout(timeOut);
-
-
+	console.log(this);
+	userPick = $(this).val();
+	console.log(userPick);
+	if (userPick == questOne.cAns)
+	{
+		correctGuess++;
+		console.log(correctGuess);
+		alert("Correct!");
+	}
+	else
+	{
+		incorrectGuess++;
+		console.log(incorrectGuess);
+		alert("Incorrect!");
+	}
 
 
 
